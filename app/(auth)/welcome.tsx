715b2c22a9cfb252a4866830e6692277eee3c99c
Rahul <rahulgurupal@gmail.com>
2025-02-06 18:32:26 +0530

    feat: add CustomButton component and integrate into onboarding flow

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,3 +1,4 @@
+import CustomButton from "@/components/CustomButtons";
 import { onboarding } from "@/constants";
 import { router } from "expo-router";
 import { useRef, useState } from "react";
@@ -6,8 +7,9 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import Swiper from "react-native-swiper";
 
 const Onboarding = () => {
-  const swiperRef = useRef(null);
+  const swiperRef = useRef<Swiper>(null);
   const [activeIndex, setActiveIndex] = useState(0);
+  const isLastSlide = activeIndex === onboarding.length - 1;
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
@@ -29,13 +31,13 @@ const Onboarding = () => {
         }
       >
         {onboarding.map((item) => (
-          <View key={item.id} className=" flex items-center justify-center p-5">
+          <View key={item.id} className="flex items-center justify-center p-5">
             <Image
               source={item.image}
               className=" w-full h-[300px]"
               resizeMode="contain"
             />
-            <View className="flex flex-row items-center justify-center w-full mt-10">
+            <View className="flex flex-row items-center justify-centermt-10">
               <Text className=" text-black text-3xl font-bold mx-10 text-center">
                 {item.title}
               </Text>
@@ -46,6 +48,16 @@ const Onboarding = () => {
           </View>
         ))}
       </Swiper>
+
+      <CustomButton
+        title={isLastSlide ? "Get Started" : "Next"}
+        onPress={() =>
+          isLastSlide
+            ? router.replace("/(auth)/sign-up")
+            : swiperRef.current?.scrollBy(1)
+        }
+        className=" w-11/12 mt-10 mb-5 "
+      />
     </SafeAreaView>
   );
 };
